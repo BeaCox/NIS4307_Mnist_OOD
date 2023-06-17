cf82302d87ca53260fae38a052d6a35731f2d3fa
unknown <3347240248@qq.com>
2023-06-17 23:26:35 +0800

    ultimate OOD-Detection version

--- test_mnist.py
+++ test_mnist.py
@@ -3,14 +3,23 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import numpy as np
 import oodcls
+import config
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
 
-correct = 0
 oodcls = oodcls.OodCls()
+
+# 超参数定义
+cfg = config.load()
+batch_size = cfg['batch_size_test']
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu')
+
 mnist_data = datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
-test_loader = DataLoader(mnist_data,batch_size=1000,shuffle=False)
+test_loader = DataLoader(mnist_data,batch_size=batch_size,shuffle=False)
+
+correct = 0
 for data ,target in test_loader:
     data, target = data.to(device), target.to(device)
     preds = oodcls.classify(data)
